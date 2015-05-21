dff925dbf97bf499029377befa0983db300c2973
skytthe <skytthe@gmail.com>
2015-05-21 12:57:32 +0200

    instantiate new modules

---
+++ sliding_average.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:56:31 05/21/2015 
+-- Design Name: 
+-- Module Name:    sliding_average - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sliding_average is
+end sliding_average;
+
+architecture Behavioral of sliding_average is
+
+begin
+
+
+end Behavioral;
+
