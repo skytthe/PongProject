c650f711983615d5669f537eecdde6469fab542d
skytthe <skytthe@Stefans-MacBook-Pro.local>
2015-08-15 06:26:41 +0200

    counts ones function added

--- PongProject/src/lib/functions.vhd
+++ PongProject/src/lib/functions.vhd
@@ -5,6 +5,7 @@ use ieee.numeric_std.all;
 package functions is
 	function log2c (n: integer) return integer;
 	function log2r (n: integer) return integer;
+	function count_ones(s : std_logic_vector) return std_logic_vector;
 end functions;
 
 package body functions is
@@ -39,5 +40,16 @@ package body functions is
 			return m;
 		end if;
 	end log2r;
+	
+	function count_ones(s : std_logic_vector) return std_logic_vector is
+		variable temp : natural := 0;
+	begin
+		for i in s'range loop
+			if s(i) = '1' then 
+				temp := temp + 1; 
+	    	end if;
+	  	end loop;
+		return std_logic_vector(to_unsigned(temp,log2r(s'range)));
+	end function count_ones;
 	
 end functions;
